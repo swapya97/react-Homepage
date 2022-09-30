6ebfaa6354727a2a56ee2101a7bbf69e54d27ef7
swapya97 <91781600+swapya97@users.noreply.github.com>
2022-09-30 01:46:08 -0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,11 @@ const Contact = _ => {
     return (
         <section>
          <h2>🤙 Contact Me</h2>
-         <a href="https://www.instagram.com/swapya97/?r=nametag" target="_blank">Instagram</a>
+         <a href="https://www.instagram.com/swapya97/?r=nametag" target="_blank" rel="noreferrer">Instagram</a>
          <br/>
-         <a href="https://www.linkedin.com/search/results/all/?heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADEwZtgB73ZriCw0cCX9k74QjQx8bGmPKUU&keywords=swapnil%20akulwar&origin=RICH_QUERY_SEARCH_HOME_HISTORY&sid=9Ni" target="_blank" >Linkedin</a>
+         <a href="https://www.linkedin.com/search/results/all/?heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADEwZtgB73ZriCw0cCX9k74QjQx8bGmPKUU&keywords=swapnil%20akulwar&origin=RICH_QUERY_SEARCH_HOME_HISTORY&sid=9Ni" target="_blank" rel="noreferrer" >Linkedin</a>
          <br/>
-         <a href="https://github.com/swapya97" target="_blank" >Github</a>
+         <a href="https://github.com/swapya97" target="_blank" rel="noreferrer">Github</a>
          <br/>
          </section>
         
